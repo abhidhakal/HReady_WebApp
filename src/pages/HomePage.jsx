d66b79430cfb085d13f3accef1226fefa63e87ec
Abhinav Dhakal <128499381+abhidhakal@users.noreply.github.com>
2025-06-12 17:49:44 +0545

    homepage ui fix

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,25 +1,29 @@
-import '../App.css';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Header from '/src/components/Header.jsx';
+import './css/HomePage.css';
 
 function HomePage() {
   return (
     <div className="full-screen">
-      <Header/>
+      <Header />
       <div className="home-container">
         <div className="left-side">
-          <img className="welcomepage-img" src="/welcome_img.png" alt="Welcome" />
+          <img 
+            className="welcomepage-img" 
+            src="/welcome_img.png" 
+            alt="Welcome to HReady - Smart HR Management Solution" 
+          />
         </div>
         <div className="right-side">
           <div>
             <h1>Welcome to HReady</h1>
             <h2>Your All-in-One Smart HR Management Solution</h2>
-            {/* Use Link to route to Admin Login */}
+          </div>
+          <div className="login-buttons">
             <Link to="/login">
               <button className="login-btn-employee">Login As Employee</button>
             </Link>
-            {/* Use Link to route to Admin Login */}
             <Link to="/login">
               <button className="login-btn-admin">Login As Admin</button>
             </Link>
